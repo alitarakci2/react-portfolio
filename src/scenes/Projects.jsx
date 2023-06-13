56427378d98f726483d0d09e49be6a934421ee89
alitarakci2 <alitarakci2@gmail.com>
2023-06-14 01:45:15 +0300

    Addingit status

---
+++ src/scenes/Projects.jsx
@@ -0,0 +1,108 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const Project = ({ title }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle}/>
+    </motion.div>
+  );
+};
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      {/* headings */}
+      <motion.div
+        className="md:w-2/4 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+        </div>
+        <div className="flex justify-center mt-5">
+          <LineGradient width="w-1/3" />
+        </div>
+
+        <p className="mt-10 mb-10">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </motion.div>
+      {/* projects */}
+
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          <div
+            className="flex justify-center text-center items-center 
+            p-10 bg-red max-w-[400px] text-2xl font-playfair font-semibold"
+          >
+            BEATIFUL USER INTERFACES
+          </div>
+          <Project title="Project 1" />
+          <Project title="Project 2" />
+          {/* row 2 */}
+          <Project title="Project 3" />
+          <Project title="Project 4" />
+          <Project title="Project 5" />
+          {/* row3 */}
+          <Project title="Project 6" />
+          <Project title="Project 7" />
+          <div
+            className="flex justify-center text-center items-center 
+            p-10 bg-blue max-w-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
